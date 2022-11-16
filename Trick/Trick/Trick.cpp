ae3ad955ef9ad36062c873f020f7cdf3ca7645b9
proninyarik <proninyarik1103@gmail.com>
2022-11-16 19:56:37 +0200

    Update 2
    Update 2

--- Trick/Trick/Trick.cpp
+++ Trick/Trick/Trick.cpp
@@ -7,6 +7,10 @@ int main()
 {
     std::cout << "Hello World!\n";
 }
+int main()
+{
+    std::cout << "Hello World!\n";
+}
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
